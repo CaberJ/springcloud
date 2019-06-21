1c6b33955d2cf7e48e6fc106704dd79a060ad790
Caber <851085470@qq.com>
2019-06-21 14:27:57 +0800

    优化项目结构，调整至洛阳poc之前的状态

--- spring-cloud-zuul-service/src/main/java/cn/caber/springcloudzuulservice/SpringCloudZuulServiceApplication.java
+++ spring-cloud-zuul-service/src/main/java/cn/caber/springcloudzuulservice/SpringCloudZuulServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SpringCloudZuulServiceApplication {
 
 	public static void main(String[] args) {
