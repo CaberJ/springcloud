67842473faa7be517fc755f81d9fd3a73ae865d4
caber <851085470@qq.com>
2020-11-27 15:31:29 +0800

    代码优化

--- spring-cloud-ribbon-consumer/src/main/java/cn/caber/springcloudribbonconsumer/SpringCloudRibbonConsumerApplication.java
+++ spring-cloud-ribbon-consumer/src/main/java/cn/caber/springcloudribbonconsumer/SpringCloudRibbonConsumerApplication.java
@@ -3,11 +3,8 @@ package cn.caber.springcloudribbonconsumer;
 import cn.caber.springcloudribbonconsumer.config.MyRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
